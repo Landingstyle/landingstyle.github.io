d58a6b957ea26b9b64b9f0bbbf776c429a6af0f3
Landingstyle <68011757+Landingstyle@users.noreply.github.com>
2021-06-29 01:26:08 +0300

    Add files via upload

---
+++ assets/core/ubgyj/slider.js
@@ -0,0 +1,45 @@
+let form = document.getElementById('modalForm');
+let submitBtn = document.getElementById('modalSubmit');
+let modalSuccess = document.querySelector('.modal-success');
+
+let priceBtnOne = document.getElementById('priceOne');
+let priceBtnTwo = document.getElementById('priceTwo');
+let priceBtnThree = document.getElementById('priceThree');
+let priceBtnFour = document.getElementById('priceFour');
+
+let price = "";
+
+priceBtnOne.addEventListener('click', () => {
+   price = "Разовая консультация";
+});
+
+priceBtnTwo.addEventListener('click', () => {
+   price = "Модуль 2 консультации";
+});
+
+priceBtnThree.addEventListener('click', () => {
+   price = "Модуль 5 консультаций";
+});
+
+priceBtnFour.addEventListener('click', () => {
+   price = "Модуль VIP 5 консультаций";
+});
+
+submitBtn.addEventListener('click', () => {
+   let formData = new FormData(form);
+   formData.append("price", price);
+   // for (var pair of formData.entries()) {
+   //    console.log(pair[0]+ ', ' + pair[1]);
+   // }
+   fetch('assets/form/sendmail.php', {
+      method: 'POST',
+      body: formData
+   }).then(function (response) {
+      // console.log(response)
+      if (response.ok) {
+         modalSuccess.innerHTML = '<p><strong>Заявка отправлена! Мы перезвоним Вам в ближайшее время!</strong></p>'
+      }
+   }).catch(function (error) {
+      // console.log(error);
+   });
+});
