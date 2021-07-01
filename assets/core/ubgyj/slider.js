8fb82e3f01180d2be268ff7bc37bf03553b5d0a8
Landingstyle <68011757+Landingstyle@users.noreply.github.com>
2021-07-02 01:17:48 +0300

    Update slider.js

--- assets/core/ubgyj/slider.js
+++ assets/core/ubgyj/slider.js
@@ -7,11 +7,13 @@ let priceBtnTwo = document.getElementById('priceTwo');
 let priceBtnThree = document.getElementById('priceThree');
 let priceBtnFour = document.getElementById('priceFour');
 
-let price = "";
-
+//Функция закрытия модалки
 function closeModal() {
    $('#formModal').modal('hide');
 }
+
+//Присвоение услуге по вызову модалки
+let price = "";
 
 priceBtnOne.addEventListener('click', () => {
    price = "Разовая консультация";
@@ -29,23 +31,78 @@ priceBtnFour.addEventListener('click', () => {
    price = "Модуль VIP 5 консультаций";
 });
 
+//Проверка обязательных полей
+function formValidate(form) {
+let error = 0;
+let formReq = form.querySelectorAll('._req');
+
+for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+
+      if (input.classList.contains('_email')) {
+         if (emailTest(input)) {
+            formAddError(input);
+            error++;
+         }
+      }else if (input.getAttribute('type') == "checkbox" && input.checked === false) {
+         formAddError(input);
+         error++;
+      }else {
+         if (input.value === '') {
+            formAddError(input);
+            error++;
+         }
+      }
+}
+
+return error;
+}
+
+//Функция добавления класса _error
+function formAddError(input) {
+input.parentElement.classList.add('_error');
+input.classList.add('_error');
+}
+
+//Функция удаления класса _error
+function formRemoveError(input) {
+input.parentElement.classList.remove('_error');
+input.classList.remove('_error');
+}
+
+//Функция проверки email
+function emailTest(input) {
+   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+
+//Отправление и валидация
 submitBtn.addEventListener('click', () => {
+   let error = formValidate(form);
    let formData = new FormData(form);
    formData.append("price", price);
    for (var pair of formData.entries()) {
       console.log(pair[0]+ ', ' + pair[1]);
    }
+   console.log(error)
+
+   if (error === 0) {
+      fetch('https://shulginhypnos.ru/assets/form/sendmail.php', {
+         method: 'POST',
+         body: formData
+      }).then(function (response) {
+         console.log(response)
+         if (response.ok) {
+            modalSuccess.innerHTML = '<p style="color: #b2ebf2;"><strong>Заявка отправлена! Мы перезвоним Вам в ближайшее время!</strong></p>'
+            setTimeout(closeModal, 1000);
+         }
+      }).catch(function (error) {
+         console.log(error);
+      });
+   } else {
+      modalSuccess.innerHTML = '<p style="color: #ff4081;"><strong>Пожалуйста, заполните корректно все обязательные поля.</strong></p>'
+   }
    
-   fetch('https://shulginhypnos.ru/assets/form/sendmail.php', {
-      method: 'POST',
-      body: formData
-   }).then(function (response) {
-      console.log(response)
-      if (response.ok) {
-         modalSuccess.innerHTML = '<p><strong>Заявка отправлена! Мы перезвоним Вам в ближайшее время!</strong></p>'
-         setTimeout(closeModal, 1000);
-      }
-   }).catch(function (error) {
-      console.log(error);
-   });
+      modalSuccess.innerHTML = '<p style="color: #ff4081;"><strong>Пожалуйста, заполните корректно все обязательные поля.</strong></p>'
+   
 });
